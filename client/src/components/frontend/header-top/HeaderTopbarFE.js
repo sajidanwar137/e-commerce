dfec3a03ae94c09f2529d2da5a3fef71fe3ee37b
sajidanwar137 <sajidanwar137@gmail.com>
2023-06-16 21:16:46 +0530

    User logout integrated

--- client/src/components/frontend/header-top/HeaderTopbarFE.js
+++ client/src/components/frontend/header-top/HeaderTopbarFE.js
@@ -1,17 +1,41 @@
-import React, {useState} from 'react'
-import { useSelector } from 'react-redux';
+import React, {useState, useEffect, useRef } from 'react'
+import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import UserLogin from '../login/UserLogin'
 import UserSignup from '../signup/UserSignup'
 import UserForgotPassword from '../userforgotpassword/UserForgotPassword'
+import { userLogout } from '../../../store/userauth/actions';
+import api from 'api/api';
+import Avtar from 'resources/images/avtar.jpeg';
 import './index.scss';
 
 const HeaderTopbarFE = () => {
+  const dispatch = useDispatch()
   const [loginModal, setLoginModal] = useState(false);
   const [signupModal, setSignupModal] = useState(false);
   const [forgotPasswordModal, setForgotPasswordModal] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef(null);
 
-  const userauth = useSelector((state) => state?.userauth?.isAuthenticated);
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleClickOutside = (event) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('click', handleClickOutside);
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
+
+  const userauth = useSelector((state) => state?.userauth);
+  const userData = useSelector((state) => state?.user?.data);
 
   const loginModalTrigger = () => {
     setLoginModal(true)
@@ -43,7 +67,20 @@ const HeaderTopbarFE = () => {
   const handleForgotPasswordModal = () => {
     setForgotPasswordModal(false)
   }
-  
+  const userLogoutHandler = async () => {
+    const payload = {user_id: userData?._id};
+    const token = userauth?.token;
+    try {
+      const result = await api.post('/guest/userlogout', payload, {headers: {
+          Authorization: `Bearer ${token}`,
+        }});
+      if (result && result.success === true) {
+        dispatch(userLogout({ userauth: result }));
+      }
+    } catch (error) {
+      console.error('Error fetching data:', error.message);
+    }
+  }
   return (
     <>
       <div className='dc-header-top py-5'>
@@ -70,14 +107,25 @@ const HeaderTopbarFE = () => {
                       <span>Wish List</span>
                     </Link>
                   </li>
-                  {userauth && <li>
-                    <Link className='d-flex align-items-center'>
-                      <span className='dc-icon-administrator'></span>
-                      <span>My account</span>
-                    </Link>
+                  {userauth?.isAuthenticated && <li>
+                    <div className='dc-header-top__dropdown d-flex align-items-center' ref={dropdownRef}>
+                      <div className='d-flex align-items-center dc-header-top__dropdown-button' onClick={toggleDropdown}>
+                        <div className='dc-header-top__dropdown--avtar d-flex align-items-center'>
+                          <img src={Avtar} alt="" />
+                        </div>
+                        <span>Hi! {userData?.name.split(' ')[0]}</span>
+                        <span className='dc-icon-arrow-down'></span>
+                      </div>
+                      <div className={`pt-8 dc-header-top__dropdown-content ${isOpen ? 'dc-header-top__dropdown-content--open' : ''}`}>
+                        <ul>
+                          <li className='px-5'><Link className='py-2 d-flex justify-content-start align-items-center'><span className='dc-icon-setting'></span><span>Profile</span></Link></li>
+                          <li className='px-5'><Link className='py-2 d-flex justify-content-start align-items-center' onClick={userLogoutHandler}><span className='dc-icon-logout'></span><span>Logout</span></Link></li>
+                        </ul>
+                      </div>
+                    </div>
                   </li>
                   }
-                  {!userauth && <li>
+                  {!userauth?.isAuthenticated && <li>
                     <Link onClick={loginModalTrigger} className='d-flex align-items-center'>
                       <span className='dc-icon-key'></span>
                       <span>Login</span>
--- client/src/store/user/actions.js
+++ client/src/store/user/actions.js
@@ -1,10 +1,10 @@
-import { SAVE, REMOVE } from "./types";
+import { USER_SAVE, USER_REMOVE } from "./types";
 
 export const save = (payload) => (dispatch) => {
-    dispatch({ type: SAVE, payload });
+    dispatch({ type: USER_SAVE, payload });
 };
 
 export const remove = () => (dispatch) => {
-    dispatch({ type: REMOVE });
+    dispatch({ type: USER_REMOVE });
 };
 
