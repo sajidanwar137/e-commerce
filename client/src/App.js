39e9a7b576bbfacc1ef87a9654c563b3353eb3d2
sajidanwar137 <sajidanwar137@gmail.com>
2023-06-18 19:49:54 +0530

    File created for Address module

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import UserProfileFE from './pages/frontend/UserProfileFE'
 import UserAvtar from './pages/frontend/UserAvtar'
 import UserUpdateProfile from './pages/frontend/UserUpdateProfile'
 import UserUpdatePassword from './pages/frontend/UserUpdatePassword'
+import UserUpdateAddress from './pages/frontend/UserUpdateAddress'
 
 import AdminChangePassword from './pages/admin/AdminChangePassword'
 import AdminForgotPassword from './pages/admin/AdminForgotPassword'
@@ -29,6 +30,7 @@ function App() {
           <Route path='/account/update/avtar/:id' element={<UserAvtar/>}/>
           <Route path='/account/update/profile/:id' element={<UserUpdateProfile/>}/>
           <Route path='/account/update/password/:id' element={<UserUpdatePassword/>}/>
+          <Route path='/account/update/address/:id' element={<UserUpdateAddress/>}/>
         </Route>
         <Route exact path='/' element={<PrivateRoutes />}>
           <Route path='/dashboard' element={<AdminDashboard/>}/>
---
+++ client/src/pages/frontend/UserUpdateAddress.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import HeaderFE from '../../components/frontend/header/HeaderFE'
+import NavbarFE from '../../components/frontend/navbar/NavbarFE'
+import BreadcrumbFE from '../../components/frontend/breadcrumb/BreadcrumbFE'
+import UserSidebarFE from '../../components/frontend/user-sidebar/UserSidebarFE'
+import FooterFE from '../../components/frontend/footer/FooterFE'
+import UpdateUserAddress from 'components/frontend/user-update-address/UpdateUserAddress'
+
+const UserUpdateAddress = () => {
+  return (
+    <>
+      <HeaderFE/>
+      <NavbarFE/>
+      <BreadcrumbFE/>
+      <div className='dc-body-container py-25'>
+        <div className='dc-container'>
+          <div className='row g-25'>
+            <div className='col-md-3'>
+              <UserSidebarFE/>
+            </div>
+            <div className='col-md-9'>
+              <div className="dc-dashboard__layout--card-body p-25">
+                <UpdateUserAddress/>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <FooterFE/>
+    </>
+  )
+}
+export default UserUpdateAddress
