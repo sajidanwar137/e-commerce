f6ed49cb25064e7bbf17278e0ae849e912dff591
sajidanwar137 <sajidanwar137@gmail.com>
2023-07-21 22:12:03 +0530

    Textarea added

--- client/src/components/common/datepicker/Datepicker.js
+++ client/src/components/common/datepicker/Datepicker.js
@@ -4,16 +4,12 @@ import './index.scss';
 
 const Datepicker = () => {
   const [startDate, setStartDate] = useState(new Date());
-  const ExampleCustomInput = forwardRef(({ value, onClick }, ref) => (
-    <button className="dc-datepicker__default border" onClick={onClick} ref={ref}>
-      {value}
-    </button>
-  ));
   return (
     <div className="dc-datepicker">
       <DatePicker 
         selected={startDate} 
         onChange={(date) => setStartDate(date)}
+        placeholderText={"MM/DD/YYYY"}
       />
     </div>
   );
